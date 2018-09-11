12c8e7b7c8222d7624407ee80bb75981e17169e8
chzzzz <37997496+chzzzz@users.noreply.github.com>
2018-09-11 21:51:13 +0800

    Add files via upload

---
+++ Homework1/program1/program1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            while (a != 0)
+            {
+                Console.WriteLine("请输入第一个数：");
+                string N1 = Console.ReadLine();
+                Console.WriteLine("请输入第二个数：");
+                string N2 = Console.ReadLine();
+                int n1 = Convert.ToInt32(N1);
+                int n2 = Convert.ToInt32(N2);
+                Console.WriteLine("他们的乘积为：");
+                Console.WriteLine("{0}",n1*n2);
+                Console.WriteLine("输入0退出，继续任意按键");
+                string j= Console.ReadLine();
+                a = Convert.ToInt32(j);
+            }
+        }
+    }
+}
---
+++ Homework1/program2/program2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((this.textBox1.Text != null)&& (this.textBox1.Text != null))
+            {
+                Int64 n1 = Convert.ToInt64(this.textBox1.Text);
+                Int64 n2 = Convert.ToInt64(this.textBox2.Text);
+                this.label4.Text = Convert.ToString(n1 * n2);
+            }
+
+        }
+    }
+}
