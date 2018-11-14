71150d82f0089430c857decdbcb1471dee92db84
啸 雷 <1003156669@qq.com>
2018-11-14 20:02:01 +0800

    Homework8

---
+++ Homework8/Homework8/Order.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace project1
+{
+    [Serializable]
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+        public Order(int orderDetailNu, string name, int orderNu)
+        {
+            orderDetails = new OrderDetails[orderDetailNu];
+            this.Name = name;
+            this.OrderNu = orderNu;
+            this.serialNumber = System.DateTime.Now.ToShortDateString();
+            if (orderNu < 10)
+            {
+                this.serialNumber = this.serialNumber + '0' + '0' + orderNu.ToString();
+            } else if (orderNu < 100)
+            {
+                this.serialNumber = this.serialNumber + '0' + orderNu.ToString();
+            } else
+            {
+                this.serialNumber = this.serialNumber + orderNu.ToString();
+            }
+            int count = 0;
+            foreach (char a in this.serialNumber)
+            {
+                if (a == '/')
+                {
+                    this.serialNumber = this.serialNumber.Remove(count, 1);
+                    count--;
+                }
+                count++;
+            }
+        }
+
+        public string serialNumber;
+        public string Name { get; set; }
+        public string SerialNumber
+        {
+            get
+            {
+                return this.serialNumber;
+            }
+        }
+        public int OrderNu { get; set; }
+
+        public string TellNu{get; set;}
+        public OrderDetails[] orderDetails;
+        public bool JudgeSerialNu(string s)
+        {
+            DateTime time = System.DateTime.Now;
+            string pattern = "^" +
+                time.Year.ToString() + 
+                time.Month.ToString() + 
+                time.Day.ToString()+
+                "[0-9]{3}$"
+                ;
+            if (Regex.IsMatch(s, pattern)) return true;
+            else return false; 
+        }
+
+    }
+}
---
+++ Homework8/Homework8/orderXslt.xslt
@@ -0,0 +1,42 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+  <xsl:output omit-xml-declaration="yes"/>
+  <xsl:template match="/">
+   <html>
+    <body>
+        <xsl:for-each select="ArrayOfOrder/Order"> 
+          <h2>
+            订单号：<xsl:value-of select="serialNumber"/>
+          </h2>
+          <h3>姓名：<xsl:value-of select="Name"/>></h3>
+          <h3>
+            电话号码：<xsl:value-of select="TellNu"/>
+          </h3>>
+          <h3>订单明细：</h3>
+          <table border="1">
+            <tr bgcolor="#9acd32">
+              <th>名称</th>
+              <th>价格</th>
+              <th>数量</th>
+            </tr>
+          <xsl:for-each select="orderDetails/OrderDetails">
+          <tr>  
+            <td>  
+              <xsl:value-of select="name"/>  
+            </td>  
+            <td>  
+              <xsl:value-of select="eachPrice"/>  
+            </td>  
+            <td>  
+              <xsl:value-of select="Nu"/>  
+            </td>  
+          </tr> 
+          </xsl:for-each>
+          </table>
+        </xsl:for-each>
+    </body>
+  </html>
+  </xsl:template>
+</xsl:stylesheet>
